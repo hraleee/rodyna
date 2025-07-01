b74c3691426ba24fc9f7c1f8e3e2395afc71072e
hraleee <alessandro.hrebinskyy@fides.it>
2025-07-01 14:28:49 +0200

    change google settings for analytics 2.0

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import { SiteHeader } from "@/components/site-header";
 import { SidebarProvider, SidebarInset, SidebarTrigger } from "@/components/ui/sidebar";
 import { Footer } from "@/components/Footer/footer";
 import { GoogleAnalytics } from "@/components/Google-analytics/google-analytics";
+import Script from "next/script";
 
 
 const geistSans = localFont({
@@ -33,11 +34,31 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-     
+     <head>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-T5TXTXQK');`,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <GoogleAnalytics />
+
+<noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T5TXTXQK"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <SidebarProvider>
           {/* Header mobile sticky con hamburger */}
           <div className="fixed top-0 left-0 z-30 w-full bg-[#1a237e] h-16 flex items-center justify-between px-4 md:hidden">
