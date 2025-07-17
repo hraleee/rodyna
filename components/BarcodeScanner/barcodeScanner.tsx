f947c2ec9180c1f0c3cddd70bc86e36c406605b1
hraleee <alessandro.hrebinskyy@fides.it>
2025-07-17 12:34:07 +0200

    fix scanner resolution

--- components/BarcodeScanner/barcodeScanner.tsx
+++ components/BarcodeScanner/barcodeScanner.tsx
@@ -12,18 +12,27 @@ export default function BarcodeScanner({ onScanned }: { onScanned: (value: strin
   const [data, setData] = useState("No result");
 
   return (
-    <div>
-      <BarcodeScannerComponent
-        width={300}
-        height={300}
-        onUpdate={(err, result) => {
-          if (result) {
-            setData(result.getText());
-            onScanned(result.getText());
-          }
-        }}
-      />
-      <p>Risultato: {data}</p>
+    <div className="flex flex-col items-center">
+      <div style={{
+        border: data !== "No result" ? "3px solid green" : "3px solid #ccc",
+        display: "inline-block",
+        borderRadius: 8
+      }}>
+        <BarcodeScannerComponent
+          width={400}
+          height={400}
+          onUpdate={(err, result) => {
+            if (result) {
+              setData(result.getText());
+              onScanned(result.getText());
+            }
+          }}
+        />
+      </div>
+      <p className="mt-2 text-gray-600 text-center text-sm">
+        Tieni il barcode a circa 20cm dalla fotocamera e muovi lentamente per mettere a fuoco.<br />
+        Risultato: <b>{data}</b>
+      </p>
     </div>
   );
 }
